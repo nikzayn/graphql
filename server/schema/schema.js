757c22e299c0d1fae62b6fa13743dcc75e811955
nikhil.vaidyar <nikhil@mail.exlmia.io>
2019-05-21 15:07:42 +0000

    Schema Update

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,17 +1,17 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
 
 //Dummy data
 var books = [
-	{name:'The Winter Soldier', genre: 'Sci-Fi', id: '1'},
-	{name:'Avengers Assemble', genre: 'Sci-Fi', id: '2'},
-	{name:'Avengers: End Game', genre: 'Sci-Fi', id: '3'},
+	{name:'The Winter Soldier', genre: 'Sci-Fi', id: '1', authordId: '1'},
+	{name:'Avengers Assemble', genre: 'Sci-Fi', id: '2', authorId: '2'},
+	{name:'Avengers: End Game', genre: 'Sci-Fi', id: '3', authorId: '3'},
 ];
 
 var authors = [
-	{name: 'Roosevelt', age: 55, id: '1'},
+	{name: 'Michael Bay', age: 55, id: '1'},
 	{name: 'Isaac Newton', age: 33, id: '2'},
 	{name: 'Anny Goldman', age: 28, id: '3'},
 ];
@@ -21,7 +21,14 @@ const BookType = new GraphQLObjectType ({
 	fields: () => ({
 	  id: {type: GraphQLID},
 	  name: {type: GraphQLString},
-	  genre: {type: GraphQLString}
+	  genre: {type: GraphQLString},
+	  author: {
+	    type: AuthorType,
+	    resolve(parent, args){
+		console.log(parent);
+		return _.find(authors, {id: parent.authorId});
+	    }
+	  }
 	})
 });
 
@@ -31,7 +38,7 @@ const AuthorType = new GraphQLObjectType ({
 	fields: () => ({
 		id: {type: GraphQLID},
 		name: {type: GraphQLString},
-		age: {type: GraphQLString}
+		age: {type: GraphQLInt}
 	})
 
 });
